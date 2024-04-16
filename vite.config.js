a9b3cc810d0d45ed51d988d03bbc7508cf5af582
jsneath <jamessneath1@gmail.com>
2024-04-16 22:34:01 +0100

    vite issue

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/project-management/", // Ensures paths are prefixed correctly
+  base: "/", // No path prefix needed
 });

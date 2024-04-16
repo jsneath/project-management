24aaf71265168bb2fccd5c1e3fae651f14f8950b
jsneath <jamessneath1@gmail.com>
2024-04-16 22:11:57 +0100

    added vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "/project-management/", // Set the base to match your GitHub repository path
+export default defineConfig(({ mode }) => {
+  const isProduction = mode === "production";
+  return {
+    plugins: [react()],
+    base: isProduction ? "/project-management/" : "/", // Adjust base path based on the environment
+  };
 });

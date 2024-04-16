fb43392946256d3767c01dee8abe71a9cda736f6
jsneath <jamessneath1@gmail.com>
2024-04-16 22:25:19 +0100

    Update index and config files

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProduction = mode === "production";
-  return {
-    plugins: [react()],
-    base: isProduction ? "/project-management/" : "/", // Adjust base path based on the environment
-  };
+export default defineConfig({
+  plugins: [react()],
+  base: "/project-management/", // Ensures paths are prefixed correctly
 });
